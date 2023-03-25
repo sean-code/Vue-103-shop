f6dbe462fddd86a50417243d1bb4c45fe0093cc3
sean-code <john.nganga@student.moringaschool.com>
2023-03-25 14:37:03 +0300

    Update: import all the styles and scripts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.slim.min.js";
+import "popper.js/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "../public/assets/css/style.css";
+import "../public/assets/fonts/style.css";
 createApp(App).mount('#app')
